62f4fd68bed8bba6c3602a7b08ecf2728afb8e60
Todd Chen <toddchen@nd.com.cn>
2019-06-19 15:32:39 +0800

    feat:  basic apollo demo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+import Currency from './components/currency'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: "https://48p1r2roz4.sse.codesandbox.io"
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Currency />
+    </div>
+  </ApolloProvider>
+);
+
+render(<App />, document.getElementById("root"));
